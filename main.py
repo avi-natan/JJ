76abb17f15c5f73d90e6936465139193eccfb208
Avraham Natan <avinat123@gmail.com>
2023-01-07 20:00:24 +0200

    small printout addition

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ def runExperimentBundle(filename):
                                             print(
                                                 f'######################## simulating instance {instance_number}/{total_instances} ########################')
                                             print(
-                                                f'pl:{pl}, an: {an}, fan: {fan}, fp:{fp}')
+                                                f'pl:{pl}, an: {an}, fan: {fan}, fp:{fp}, fd:{fd}, fen:{fen}, rn:{rn}')
                                             annotated_observation, spdchgtab, simulation_remake = \
                                                 simulator2.simulate_instance(bs, plan, F, fp, fsr, ft, fd, rn + 1,
                                                                              instance_number, total_instances)
